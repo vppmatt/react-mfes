572eeaea39f6a6a6a730c28bb788cbfb3b07fb47
Matt Thornfield <matt@multicode.co.uk>
2024-03-07 12:18:25 +0000

    fix to App.tsx for routing

--- 05 routing/post-exercise/payments-list/src/App.tsx
+++ 05 routing/post-exercise/payments-list/src/App.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import './App.css';
 import PaymentsList from './PaymentsList/PaymentsList';
 import FindTransactionPage from './PaymentsList/FindTransactionPage';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
+    <BrowserRouter>
       <h1>Payments list app</h1>
       <FindTransactionPage serverUrl='http://someServer' onNavigate={() => {}} initialPath={[]}/>
-    </div>
+    </BrowserRouter>
   );
 }
 
